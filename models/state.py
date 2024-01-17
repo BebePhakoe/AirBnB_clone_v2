15471f68f395fbcd923254197b0566f2456b6af9
Beloved Phakoe <Beloved.phakoe@outlook.com>
2024-01-17 15:05:17 -0800

    models

--- models/state.py
+++ models/state.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
 from models.city import City
+import shlex
 
 
-class State(BaseModel):
-    """ State class """
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
     def cities(self):
-        from models import storage
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
 
-        city_inst = [inst for inst in storage.all().values()
-                     if isinstance(inst, City)]
-        result = [cities for cities in city_inst if cities.state_id == self.id]
-        return result
